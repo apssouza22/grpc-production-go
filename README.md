60799074c67f3aa0b6421206161cb98902496751
Alexsandro Souza <alexsandro.souza@deem.com>
2019-08-31 20:33:41 +0100

    Restructured the modules

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 - Added health check service
 - Added shutdown hook
 - Added keep alive params
-- Added interceptors(auth, request canceled, execution time)
+- Added server interceptors(auth, request canceled, execution time)
 - Added idempotent check
 - Added server example
+- Added Client example (timeout, healthcheck)
+- Added client interceptor (timeout log)
--- examples/grpc/server.go
+++ examples/grpc/server.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"context"
-	grpc_server "github.com/apssouza22/grpc-server/grpc"
-	"google.golang.org/grpc/examples/helloworld/helloworld"
-	"log"
+	server "github.com/apssouza22/grpc-server/grpc/server"
 )
 
-type server struct{}
-
-func (s *server) SayHello(ctx context.Context, in *helloworld.HelloRequest) (*helloworld.HelloReply, error) {
-	log.Printf("Received: %v", in.Name)
-	return &helloworld.HelloReply{Message: "Hello " + in.Name}, nil
-}
-
 func main() {
-	grpc_server.ServerInitialization()
+	server.ServerInitialization()
 }
