36656bd87e923357f0f315b8196e3da39259b77a
Alexsandro Souza <alexsandro.souza@deem.com>
2020-01-13 18:37:40 +0000

    Add Loadbalancing test

--- server/server_example.go
+++ server/server_example.go
@@ -5,13 +5,22 @@ import (
 	interceptors "github.com/apssouza22/grpc-server-go/serverinterceptor"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/examples/helloworld/helloworld"
+	"google.golang.org/grpc/metadata"
 	"log"
+	"os"
 )
 
 type server struct{}
 
 func (s *server) SayHello(ctx context.Context, in *helloworld.HelloRequest) (*helloworld.HelloReply, error) {
 	log.Printf("Received: %v", in.Name)
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Printf("Unable to get hostname %v", err)
+	}
+	if hostname != "" {
+		grpc.SendHeader(ctx, metadata.Pairs("hostname", hostname))
+	}
 	return &helloworld.HelloReply{Message: "Hello " + in.Name}, nil
 }
 
